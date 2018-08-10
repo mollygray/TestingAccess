cac6de9ba9b6f1df9313e7ab69f9a9c53acc95ae
Jim T <james.trudeau@cypress.com>
2018-08-10 10:05:46 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "mcu.h"
+
+int main(void)
+{
+    /* Place your initialization/startup code here (e.g. Drv_Init()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
